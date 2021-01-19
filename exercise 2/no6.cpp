6a12c8b93fb6091b6bd2f62c75abb75f1eec3449
Handsome Victor <857730556@qq.com>
2021-01-19 07:58:33 +0100

    Add files via upload

--- exercise 2/no6.cpp
+++ exercise 2/no6.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 
 int fibonacci(int n);
-
+int k = 1;
 int main(){
     int n;
     cout<<"please say the n-th one that you want to see: ";
     cin >> n;
     cout<<"the number of n-th is: "<<fibonacci(n)<<endl;
+    cout<<"the number of recursive calls is: "<<k<<endl;
 }
 
 int fibonacci(int n){
@@ -18,6 +19,7 @@ int fibonacci(int n){
     }else if(n==0){
         return 0;
     }else if(n>=2){
+        k = k+1;
         return fibonacci(n-1)+fibonacci(n-2);
     }
 }
